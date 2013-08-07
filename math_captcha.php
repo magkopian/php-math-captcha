92ddcdfb5aa5ee0af16586b2e0afa6d0720a2513
magkopian <m.agkopian@gmail.com>
2013-08-07 21:08:41 +0300

    first commit

---
+++ math_captcha.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+
+	//generate the math captcha numbers and save the result in a session variable
+	$_SESSION['math_captcha'] = ( $num_1 = rand(0, 10) * rand(1, 3) ) + ( $num_2 = rand(0, 10) * rand(1, 3) );
+	
+	$img = @imagecreatetruecolor(99, 19) or die(); //create a canvas
+	
+	$color_black = imagecolorallocate($img, 0, 0, 0); //allocate the colors
+	$color_white = imagecolorallocate($img, 255, 255, 255);
+	
+	imagefilledrectangle ($img, 0, 0, 99, 19, $color_white); //make background of the image white
+	
+	imagestring($img, 10, 2, 2,  $num_1.' + '.$num_2.' = ', $color_black); //draw the question string on the image
+	
+	header('Content-Type: image/png'); //set the MIME type
+	imagepng($img); //send the image
+	imagedestroy($img); //free the allocated memory of the image
+?>
---
+++ test_form.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+
+	$msg = '';
+	if (isset($_POST['submit']) && !empty($_POST['submit']) && $_POST['submit'] == 'Submit') { //check if form has been submited
+		if (!isset($_SESSION['math_captcha']) || empty($_SESSION['math_captcha'])) { //check if math captcha has been generated
+			$msg = '<span class="error">An unexpected error has been occurred</span>';
+		}
+		else if (!isset($_POST['captcha_ans']) || empty($_POST['captcha_ans'])) { //check if user answered the question
+			$msg = '<span class="error">Please fill the answer to the math question</span>';
+		}
+		else {
+			$math_captcha = $_SESSION['math_captcha'];
+			unset($_SESSION['math_captcha']);
+			
+			if ((int) trim($_POST['captcha_ans']) === $math_captcha) { //validate the answer
+				$msg = '<span class="success">SUCCESS</span>'; //in a real application here you can register, loggin your user etc
+			}
+			else {
+				$msg = '<span class="error">You didn\'t answered the question correctly</span>';
+			}
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Simple Chat</title>
+		<style type="text/css">
+			.error {
+				color: red;
+			}
+			.success {
+				color: green;
+			}
+		</style>
+	</head>
+	<body>
+		<p id="msg"><?=$msg?></p>
+		<p>Answer to this simple math question:</p>
+		<form action="" method="POST">
+			<img src="captcha.png" alt="">
+			<input type="text" name="captcha_ans">
+			<input type="submit" name="submit" value="Submit">
+		</form>
+	</body>
+<html>
