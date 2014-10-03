3378ee56b2a20c091e7f79aa136388dfb078b814
magkopian <m.agkopian@gmail.com>
2014-10-04 00:22:31 +0300

    Reformated code + fixed some mistakes in comments

--- math_captcha.php
+++ math_captcha.php
@@ -6,19 +6,19 @@
 
 session_start();
 
-//generate the math captcha numbers and save the result in a session variable
+// Generate the math captcha numbers and save the result in a session variable
 $_SESSION['math_captcha'] = ( $num_1 = rand(0, 10) * rand(1, 3) ) + ( $num_2 = rand(0, 10) * rand(1, 3) );
 
-$img = @imagecreatetruecolor(99, 19) or die(); //create a canvas
+$img = @imagecreatetruecolor(99, 19) or die(); // Create a canvas
 
-$color_black = imagecolorallocate($img, 0, 0, 0); //allocate the colors
+$color_black = imagecolorallocate($img, 0, 0, 0); // Allocate the colors
 $color_white = imagecolorallocate($img, 255, 255, 255);
 
-imagefilledrectangle ($img, 0, 0, 99, 19, $color_white); //make background of the image white
+imagefilledrectangle($img, 0, 0, 99, 19, $color_white); // Make the background of the image white
 
-imagestring($img, 10, 2, 2,  $num_1.' + '.$num_2.' = ', $color_black); //draw the question string on the image
+imagestring($img, 10, 2, 2,  $num_1.' + '.$num_2.' = ', $color_black); // Draw the math question on the image
 
-header('Content-Type: image/png'); //set the MIME type
-imagepng($img); //send the image
-imagedestroy($img); //free the allocated memory of the image
-?>
+header('Content-Type: image/png'); // Set the MIME type to image/png
+imagepng($img); // Send the image to the browser
+
+imagedestroy($img); // Free the allocated memory of the image
--- test_form.php
+++ test_form.php
@@ -7,25 +7,37 @@
 session_start();
 
 $msg = '';
-if ( isset($_POST['submit']) && !empty($_POST['submit']) && $_POST['submit'] == 'Submit' ) { //check if form has been submited
 
-	if ( !isset($_SESSION['math_captcha']) || empty($_SESSION['math_captcha']) ) { //check if math captcha has been generated
+// Check if form has been submited
+if ( isset($_POST['submit']) && !empty($_POST['submit']) ) {
+
+	// Check if math captcha has been generated
+	if ( !isset($_SESSION['math_captcha']) || empty($_SESSION['math_captcha']) ) {
+		
 		$msg = '<span class="error">An unexpected error has been occurred</span>';
-	}
-	else if ( !isset($_POST['captcha_ans']) || empty($_POST['captcha_ans']) ) { //check if user answered the question
+		
+	}// Check if user answered the question
+	else if ( !isset($_POST['captcha_ans']) || empty($_POST['captcha_ans']) ) {
+		
 		$msg = '<span class="error">Please fill the answer to the math question</span>';
+		
 	}
 	else {
 	
 		$math_captcha = $_SESSION['math_captcha'];
 		unset($_SESSION['math_captcha']);
 		
-		if ( (int) trim($_POST['captcha_ans']) === $math_captcha ) { //validate the answer
-			//in a real application here you can register, login your user, etc
+		// Validate the answer
+		if ( (int) trim($_POST['captcha_ans']) === $math_captcha ) {
+			
+			// In a real application here you can register/login the user, insert a comment in the database etc
 			$msg = '<span class="success">SUCCESS</span>';
+			
 		}
 		else {
+			
 			$msg = '<span class="error">You didn\'t answered the question correctly</span>';
+			
 		}
 		
 	}
@@ -36,7 +48,7 @@ if ( isset($_POST['submit']) && !empty($_POST['submit']) && $_POST['submit'] == 
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Simple Chat</title>
+		<title>PHP Math Captcha</title>
 		<style type="text/css">
 			.error {
 				color: red;
@@ -47,7 +59,7 @@ if ( isset($_POST['submit']) && !empty($_POST['submit']) && $_POST['submit'] == 
 		</style>
 	</head>
 	<body>
-		<p id="msg"><?=$msg?></p>
+		<p id="msg"><?php echo $msg; ?></p>
 		<p>Answer to this simple math question:</p>
 		<form action="" method="POST">
 			<img src="captcha.png" alt="">
@@ -55,4 +67,4 @@ if ( isset($_POST['submit']) && !empty($_POST['submit']) && $_POST['submit'] == 
 			<input type="submit" name="submit" value="Submit">
 		</form>
 	</body>
-<html>
+</html>
